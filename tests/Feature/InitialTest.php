0156a3b884d35dcabea8038d4cb7660e952d7ebc
= <justijndepover@gmail.com>
2020-10-09 16:20:14 +0200

    added tests

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Justijndepover\Scaffolding\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    public function it_should_assert_true()
+    {
+        $this->assertTrue(true);
+    }
+}
