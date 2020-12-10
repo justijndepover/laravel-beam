406639c747a6286e6cf85af37136093cef4e4ad3
Justijn Depover <justijndepover@gmail.com>
2020-12-10 08:01:43 +0100

    wip

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -32,10 +32,13 @@ class InstallCommand extends Command
             $this->executeLater(LaravelInboxCommand::class);
         }
 
-        // $this->confirm('Do you want to install laravel-cookie-consent?', true);
-        // $this->choice('What css framework do you want to install?', ['Tailwindcss', 'Bootstrap', 'None'], 0);
-        // $this->confirm('Do you want to setup basic authentication?', true);
-        // $this->confirm('Do you want to install laravel-cms?', true);
+        if ($this->confirm('Do you want to install laravel-cookie-consent?', true)) {
+            //
+        }
+
+        if ($this->choice('What css framework do you want to install?', ['Tailwindcss', 'Bootstrap', 'None'], 0)) {
+            //
+        }
 
         $this->info('Installing the assets');
         $this->executeCommands();
